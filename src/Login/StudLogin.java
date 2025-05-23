bb27d07e2502ae0698fb0f092188cd8d1e428b1f
SidduQA <siddarth2756@gmail.com>
2025-05-23 18:53:10 +0530

    Created login class

---
+++ src/Login/StudLogin.java
@@ -0,0 +1,14 @@
+package Login;
+
+public class StudLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name="sid";
+		
+		System.out.println("Login successfull "+name);
+
+	}
+
+}
